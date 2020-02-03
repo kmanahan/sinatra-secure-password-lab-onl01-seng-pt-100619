ac4cf86fb64457886c6daa2893c2c6cd41724ea5
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-03 15:34:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,10 +36,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    if params[:username] == "" || params[:password] ==""
-    redirect "/failure"
-    else 
-      redirect "/account"
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/success"
+    else
+      redirect "/failure"
     end
   end
 
