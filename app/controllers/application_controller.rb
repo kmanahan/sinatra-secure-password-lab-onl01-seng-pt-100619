4514fd8682ae7b2e3e07cc501d8d4d0d7fb0204c
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-03 15:23:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
     if user.save
+      binding.pry
     redirect "/login"
     else
       redirect "/failure"
